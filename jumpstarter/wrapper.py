01686262c4396abb8908a5073b3881a5989bf011
Nick Cao <nickcao@nichi.co>
2026-02-26 12:06:06 -0500

    [HACK] Expand rootfs

--- jumpstarter/wrapper.py
+++ jumpstarter/wrapper.py
@@ -4,7 +4,19 @@ import time
 import sys
 import os
 import subprocess
+import importlib
+from pathlib import Path
 
+project_root = Path(__file__).parent.parent
+ssh_client_path = project_root / "infra-tests" / "ssh_client.py"
+spec = importlib.util.spec_from_file_location("ssh_client", ssh_client_path)
+ssh_client_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(ssh_client_module)
+SSHConnection = ssh_client_module.SSHConnection
+
+# FIXME
+USERNAME = "admin"
+PASSWORD = "password"
 
 with env() as client:
     with client.log_stream():
@@ -26,8 +38,15 @@ with env() as client:
             os.environ["JETSON_HOST"] = addr[0]
             os.environ["JETSON_PORT"] = str(addr[1])
 
-            # FIXME
-            os.environ["JETSON_USERNAME"] = "admin"
-            os.environ["JETSON_PASSWORD"] = "password"
+            os.environ["JETSON_USERNAME"] = USERNAME
+            os.environ["JETSON_PASSWORD"] = PASSWORD
+
+            with SSHConnection(
+                addr[0],
+                USERNAME,
+                PASSWORD,
+                addr[1],
+            ) as ssh:
+                ssh.sudo("/usr/libexec/bootc-generic-growpart")
 
             subprocess.run(sys.argv[1:])
